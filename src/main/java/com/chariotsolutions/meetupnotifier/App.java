045080c0b27e3b3cab75f6d4576b033e0ef681a7
Kyle Swartley <kyle@chariotsolutions.com>
2015-07-10 12:31:38 -0400

    Now loads meetup groups (and the api key) from a properties file, and checks upcoming events for each one. Property loading was attached to Spring.

--- src/main/java/com/chariotsolutions/meetupnotifier/App.java
+++ src/main/java/com/chariotsolutions/meetupnotifier/App.java
@@ -1,12 +1,6 @@
 package com.chariotsolutions.meetupnotifier;
 
-import com.chariotsolutions.meetupnotifier.meetup.api.MeetupQueryExecutor;
-import com.chariotsolutions.meetupnotifier.meetup.api.GoogleConverter;
-import com.google.api.services.calendar.model.Event;
-
 import com.chariotsolutions.meetupnotifier.google.CalendarSample;
-import com.chariotsolutions.meetupnotifier.meetup.api.MeetupQuery;
-import com.chariotsolutions.meetupnotifier.meetup.api.messages.Results;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
@@ -14,38 +8,23 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 
 public class App {
 
   private static final Logger logger = LoggerFactory.getLogger(App.class);
 
+  public static com.google.api.services.calendar.Calendar cal;
+
   /**
    * Currently being used for testing purposes.
    * @param args Commandline arguments
    */
-  public static void main(String[] args) {
-    logger.debug("From the main method");
+  public static void main(String[] args) throws IOException, GeneralSecurityException {
+    cal = CalendarSample.getCalendarService();
+    logger.debug("Calendar access object retrieved");
+
     ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"context.xml"});
-
-  }
-
-  public static void stuff(String[] args) throws IOException, GeneralSecurityException {
-    Results resultsObject = new MeetupQueryExecutor("591a1341d1b5e50587c3b485afa34").executeQuery(new MeetupQuery("phillypug"));
-    com.google.api.services.calendar.Calendar cal = CalendarSample.getCalendarService();
-
-    GoogleConverter googleConverter = new GoogleConverter();
-
-    List<Event> events =
-        resultsObject.getResults().stream()
-            .map(googleConverter::convertTo)
-            .collect(Collectors.toList());
-    for (Event event : events) {
-      cal.events().insert("primary", event).execute();
-    }
   }
 }
 
--- src/main/java/com/chariotsolutions/meetupnotifier/AppBeanImpl.java
+++ src/main/java/com/chariotsolutions/meetupnotifier/AppBeanImpl.java
@@ -1,11 +1,21 @@
 package com.chariotsolutions.meetupnotifier;
 
 
+import com.chariotsolutions.meetupnotifier.meetup.api.GoogleConverter;
+import com.chariotsolutions.meetupnotifier.meetup.api.MeetupQueryExecutor;
+import com.google.api.services.calendar.model.Event;
+
+import com.chariotsolutions.meetupnotifier.meetup.api.MeetupProperties;
+import com.chariotsolutions.meetupnotifier.meetup.api.MeetupQuery;
+import com.chariotsolutions.meetupnotifier.meetup.api.messages.Result;
+import com.chariotsolutions.meetupnotifier.meetup.api.messages.Results;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 @EnableScheduling
@@ -13,8 +23,16 @@ public class AppBeanImpl {
 
   private static final Logger logger = LoggerFactory.getLogger(AppBeanImpl.class);
 
-  @Scheduled(fixedRate = 60000L)
-  public void run() {
-    logger.warn("Log entry!");
+  @Scheduled(fixedRate = 60000L, initialDelay = 4000L)
+  public void run() throws IOException {
+    for (String meetupGroupName : MeetupProperties.meetupGroups) {
+      Results results = new MeetupQueryExecutor("591a1341d1b5e50587c3b485afa34").executeQuery(new MeetupQuery("phillypug"));
+
+      for (Result meetupEvent : results.getResults()) {
+        Event evt = new GoogleConverter().convertTo(meetupEvent);
+        App.cal.events().insert("primary", evt).execute();
+        logger.warn("Oh no I added a thing to the calendar!");
+      }
+    }
   }
 }
