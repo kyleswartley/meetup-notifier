774ed5e957334bc78a690d6b0a4d937368b2063f
Kyle Swartley <kyle@chariotsolutions.com>
2015-07-14 11:37:00 -0400

    Interfacified event translator, we can use lambdas now

--- src/main/java/com/chariotsolutions/meetupnotifier/meetup/api/ResultConverter.java
+++ src/main/java/com/chariotsolutions/meetupnotifier/meetup/api/ResultConverter.java
@@ -1,7 +1,6 @@
 package com.chariotsolutions.meetupnotifier.meetup.api;
 
 import com.chariotsolutions.meetupnotifier.meetup.api.messages.Result;
-import com.google.api.services.calendar.model.Event;
 
 @FunctionalInterface
 public interface ResultConverter<T> {
