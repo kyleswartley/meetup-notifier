c45fb19eafe537f3661f6b93ab0d8b93319ad6d5
Kyle Swartley <kyle@chariotsolutions.com>
2015-07-15 13:38:32 -0400

    Checker scheduling pulled out to xml and .properties file.

--- src/main/java/com/chariotsolutions/meetupnotifier/AppBeanImpl.java
+++ src/main/java/com/chariotsolutions/meetupnotifier/AppBeanImpl.java
@@ -7,13 +7,11 @@ import com.chariotsolutions.meetupnotifier.meetup.api.GoogleConverter;
 import com.chariotsolutions.meetupnotifier.meetup.api.MeetupQuery;
 import com.chariotsolutions.meetupnotifier.meetup.api.MeetupQueryExecutor;
 
-import com.chariotsolutions.meetupnotifier.meetup.api.messages.Result;
 import com.chariotsolutions.meetupnotifier.meetup.api.messages.Results;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -28,7 +26,6 @@ public class AppBeanImpl {
 
   private String[] groups;
 
-  @Scheduled(fixedRate = 60000L, initialDelay = 4000L)
   public void run() throws IOException {
     for (String meetupGroupName : groups) {
       Results results = new MeetupQueryExecutor().executeQuery(new MeetupQuery(meetupGroupName));
