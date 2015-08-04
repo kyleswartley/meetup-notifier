d1f9e14ca8841df9aa34dcd59c2f55b7c6cc4445
Kyle Swartley <kyle@chariotsolutions.com>
2015-08-04 10:20:29 -0400

    It tracks the unique IDs of meetup events and google events. It will not try to add an event to the calendar more than once. If the event is manually deleted from the calendar it will stay deleted. If the meetup event is updated, these changes will not be reflected in the google event.

--- src/main/java/com/chariotsolutions/meetupnotifier/MeetupNotifierImpl.java
+++ src/main/java/com/chariotsolutions/meetupnotifier/MeetupNotifierImpl.java
@@ -6,36 +6,52 @@ import com.google.api.services.calendar.model.Event;
 import com.chariotsolutions.meetupnotifier.meetup.api.GoogleConverter;
 import com.chariotsolutions.meetupnotifier.meetup.api.MeetupQuery;
 import com.chariotsolutions.meetupnotifier.meetup.api.MeetupQueryExecutor;
+import com.chariotsolutions.meetupnotifier.meetup.api.messages.Result;
 
-import com.chariotsolutions.meetupnotifier.meetup.api.messages.Results;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.sql.DataSource;
 
 @Component
 @EnableScheduling
 public class MeetupNotifierImpl {
 
   private static final Logger logger = LoggerFactory.getLogger(MeetupNotifierImpl.class);
+  @Autowired
+  DataSource ds;
 
   private String[] groups;
 
   public void queryAndNotify() throws IOException {
     for (String meetupGroupName : groups) {
-      Results results = new MeetupQueryExecutor().executeQuery(new MeetupQuery(meetupGroupName));
+      JdbcTemplate template = new JdbcTemplate(ds);
+      String metupInCalendarQuery = "SELECT count(*) from id_table where meetupID=?";
 
-      List<Event> events = results.getResults().stream()
+      List<Result> results = new MeetupQueryExecutor()
+          .executeQuery(new MeetupQuery(meetupGroupName)).getResults().stream()
+          .filter(t -> template.queryForObject(metupInCalendarQuery, Integer.class, t.getId()) == 0)
+          .collect(Collectors.toList());
+
+      List<Event> events = results.stream()
           .map(new GoogleConverter()::convertTo)
           .collect(Collectors.toList());
 
+      Iterator<Result> meetupIter = results.iterator();
+
       for (Event googleEvent : events) {
-        App.cal.events().insert("primary", googleEvent).execute();
+        Event insertedEvent = App.cal.events().insert("primary", googleEvent).execute();
+        template.update("INSERT INTO id_table VALUES (?, ?)",
+                        insertedEvent.getId(), meetupIter.next().getId());
         logger.debug("An event was added to the calendar successfully");
       }
     }
