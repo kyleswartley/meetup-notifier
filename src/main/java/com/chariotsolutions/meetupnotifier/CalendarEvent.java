f3a1c6ab566fcc7b031c1806a330aad6037db316
Kyle Swartley <kyle@chariotsolutions.com>
2015-08-06 09:55:12 -0400

    add preconditions

--- src/main/java/com/chariotsolutions/meetupnotifier/CalendarEvent.java
+++ src/main/java/com/chariotsolutions/meetupnotifier/CalendarEvent.java
@@ -1,10 +1,14 @@
 package com.chariotsolutions.meetupnotifier;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 public class CalendarEvent {
   private String googleId;
   private String meetupId;
 
   public CalendarEvent(String googleId, String meetupId) {
+    checkArgument(googleId.length() > 0, "The google ID should not be 0 length");
+    checkArgument(meetupId.length() > 0, "The meetup ID should not be 0 length");
     this.googleId = googleId;
     this.meetupId = meetupId;
   }
