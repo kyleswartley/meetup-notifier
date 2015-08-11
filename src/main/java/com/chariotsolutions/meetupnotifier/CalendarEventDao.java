54156e134edf95e093f560ff9b786427e9492cda
Kyle Swartley <kyle@chariotsolutions.com>
2015-08-11 11:14:34 -0400

    Extract interface.

--- src/main/java/com/chariotsolutions/meetupnotifier/CalendarEventDao.java
+++ src/main/java/com/chariotsolutions/meetupnotifier/CalendarEventDao.java
@@ -1,31 +1,11 @@
 package com.chariotsolutions.meetupnotifier;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
-public class CalendarEventDao {
+public interface CalendarEventDao {
 
-  @Autowired
-  private JdbcTemplate template;
+  List<CalendarEvent> findEventsByMeetupId(String meetupId);
 
-  void insertEvent(String googleId, String meetupId) {
-    template.update(
-        "INSERT INTO calendar_event VALUES (?, ?)", googleId, meetupId);
-  }
-
-  List<CalendarEvent> findEventsByMeetupId(String meetupId) {
-    return template.query(
-        "SELECT * from calendar_event where meetupID=?", eventMapper, meetupId);
-  }
-
-  private RowMapper<CalendarEvent> eventMapper = (resultSet, ii) -> new CalendarEvent(
-      resultSet.getString("googleID"),
-      resultSet.getString("meetupID"));
-
-
+  void insertEvent(String googleId, String meetupId);
 }
---
+++ src/main/java/com/chariotsolutions/meetupnotifier/CalendarEventDaoHsqlDbImpl.java
@@ -0,0 +1,31 @@
+package com.chariotsolutions.meetupnotifier;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CalendarEventDaoHsqlDbImpl implements CalendarEventDao {
+
+  @Autowired
+  private JdbcTemplate template;
+
+  public void insertEvent(String googleId, String meetupId) {
+    template.update(
+        "INSERT INTO calendar_event VALUES (?, ?)", googleId, meetupId);
+  }
+
+  public List<CalendarEvent> findEventsByMeetupId(String meetupId) {
+    return template.query(
+        "SELECT * from calendar_event where meetupID=?", eventMapper, meetupId);
+  }
+
+  private RowMapper<CalendarEvent> eventMapper = (resultSet, ii) -> new CalendarEvent(
+      resultSet.getString("googleID"),
+      resultSet.getString("meetupID"));
+
+
+}
