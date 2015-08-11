a818ddbc7b43fa8b67e5dfe1dd8fffa6274c8742
Kyle Swartley <kyle@chariotsolutions.com>
2015-08-11 11:23:20 -0400

    Change AO from Component to Repository.

--- src/main/java/com/chariotsolutions/meetupnotifier/CalendarEventDaoHsqlDbImpl.java
+++ src/main/java/com/chariotsolutions/meetupnotifier/CalendarEventDaoHsqlDbImpl.java
@@ -3,11 +3,11 @@ package com.chariotsolutions.meetupnotifier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Component
+@Repository
 public class CalendarEventDaoHsqlDbImpl implements CalendarEventDao {
 
   @Autowired
